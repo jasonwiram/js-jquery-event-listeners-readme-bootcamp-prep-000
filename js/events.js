98424b4c3465f23ef821c6cc0a038981be924b21
Jason Wiram <jwiram@gmail.com>
2019-04-24 14:06:44 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -13,7 +13,7 @@ function frameIt() {
 
 function pressIt() {
   $("input").on("keydown", function(key) {
-    if(key == 71) {
+    if(key.which == 71) {
       alert("You pressed G")
       return
     } else {
